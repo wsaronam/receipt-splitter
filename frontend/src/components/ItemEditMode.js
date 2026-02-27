4db38b8c417148d2eb895eb735a8913830534168
Willy Saronamihardja <wsaronam@gmail.com>
2026-02-27 00:41:26 -0800

    updates
    - added ability to edit items
    - styled the edit item stuff

--- frontend/src/components/ItemEditMode.js
+++ frontend/src/components/ItemEditMode.js
@@ -14,21 +14,40 @@ function ItemEditMode({
     onCancelEditing
 }) {
     return (
-        <>
+        <div className="item-edit-mode">
             <div className="edit-inputs">
                 <input 
+                    type='text'
+                    value={editedItems[itemIndex]?.name || item.name}
+                    onChange={(e) => onUpdateEditing(itemIndex, 'name', e.target.value)}
                     placeholder="Item name"
+                    className="edit-name-input"
                 />
                 <input
+                    number="number"
+                    value={editedItems[itemIndex]?.price || item.price}
+                    onChange={(e) => onUpdateEditing(itemIndex, 'price', e.target.value)}
                     placeholder="0.00"
+                    step="0.01"
+                    className="edit-price-input"
                 />
             </div>
             <div className="edit-actions">
-                <button>Save</button>
-                <button>Cancel</button>
+                <button
+                    onClick={() => onSaveEditing(itemIndex)}
+                    className="save-btn"
+                >
+                    Save
+                </button>
+                <button
+                    onClick={onCancelEditing}
+                    className="cancel-btn"
+                >
+                    Cancel
+                </button>
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
 
